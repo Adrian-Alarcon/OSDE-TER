8b3d68ab94feaab406be02905589669af5043ed3
Adri Alarcon <64123596+Adrian-Alarcon@users.noreply.github.com>
2022-08-23 21:03:16 -0300

    cambiando bloquea PB a NT
    Se cambia el bloque PB por NT en la toma de pedidos

--- Facturador-OSDE-TERC.py
+++ Facturador-OSDE-TERC.py
@@ -31,9 +31,9 @@ def interfaz():
 
           pythoncom.CoInitialize()
           # Crear una copia del excel padre.
-          #shutil.copy(rutas.archivo_excel, rutas.archivo_excel_trabajo)
+          shutil.copy(rutas.archivo_excel, rutas.archivo_excel_trabajo)
 
-#---------VARIABLES--------#
+          #---------VARIABLES--------#
           afiliado_anterior = None
           afiliado_actual = None
 
@@ -47,6 +47,8 @@ def interfaz():
           l_ped_ext = []
           l_convenios = []
           l_mat_cliente = []
+          l_tipo_dispone = [] # Para identificar los centros asistenciales
+          l_ce_asist = []
 
           l_mat_cliente_fact = []
           l_observ_toma = []
@@ -54,16 +56,18 @@ def interfaz():
           l_cant_facturar = []
           fecha_entrega = []
           filas_completar = []
-          fila = 9
+          tipo_dispone = [] # Para identificar los centros asistenciales
+          centro_asistencial = []
           l_filas = []
 #---------------------------#
 
           excel_trabajo = load_workbook(rutas.archivo_excel_trabajo, data_only=True)
           try:
                h_t = excel_trabajo["inicio"]
+
+               # === CARGAR LISTAS === #
                cont = 2
                max_filas = int(h_t[f"A2"].value)
-
                for i in range(2, max_filas + 1):
                     revision = h_t[f"H{i}"].value
                     if revision == "SI":
@@ -81,60 +85,78 @@ def interfaz():
                          l_dispones.append(h_t[f"N{i}"].value)
                          fecha_entrega.append(h_t[f"V{i}"].value)
                          l_convenios.append(h_t[f"Q{i}"].value)
+                         l_tipo_dispone.append(h_t[f"S{i}"].value)
+                         l_ce_asist.append(h_t[f"T{i}"].value)
                          filas_completar.append(str(i))
                print(f"Filas a completar:", filas_completar)
 
 
-               # -> FACTURACION <--
+               # ==== CARGA DE PEDIDOS ==== #
                for i in range(len(l_afiliados_sap)):
                     afiliado_actual = l_afiliados_sap[i]
 
                     if afiliado_actual == afiliado_anterior or afiliado_anterior == None:
-                         print(f"{i} - AFILIADO:{afiliado_actual} |")
                          l_observ_toma.append(l_obsersevaciones_int[i])
                          l_mat_facturar.append(l_id_productos[i])
                          l_mat_cliente_fact.append(l_mat_cliente[i])
                          l_cant_facturar.append(l_cantidades_va01[i])
+                         tipo_dispone.append(l_tipo_dispone[i])
+                         centro_asistencial.append(l_ce_asist[i])
                          l_filas.append(filas_completar[i])
 
                     elif afiliado_actual != afiliado_anterior:
-                         print(f"{i} - Afiliado Diferente")
-                         print(f" ------ SE FACTURA AF: {afiliado_anterior} ------ ")
-                         print("VA01:", l_ped_ext[i-1], l_dispones[i-1], fecha_entrega[i-1], l_mat_facturar, l_cant_facturar, l_convenios[i-1], l_mat_cliente_fact)
-                         pedidova01 = va01_2(0, l_ped_ext[i-1], l_dispones[i-1], fecha_entrega[i-1], l_mat_facturar, l_cant_facturar, l_convenios[i-1], l_mat_cliente_fact)
-                         sleep(1)
-                         print("TOMA:", "pedidova01", l_dispones[i-1], afiliado_anterior, "06", l_observ_toma)
-                         _toma = toma(0, pedidova01, l_dispones[i-1], afiliado_anterior, "06", l_observ_toma)
+                         print(f"SE CARGARA PED: {afiliado_anterior} - {i} - {tipo_dispone} - {centro_asistencial}")
+                         print("\tYENDO A VA01:", l_ped_ext[i-1], l_dispones[i-1], fecha_entrega[i-1], l_mat_facturar, l_cant_facturar, l_convenios[i-1], l_mat_cliente_fact, tipo_dispone[0], centro_asistencial[0])
 
-                         #Completar Excel con pedido generado
-                         for fila in l_filas:
-                              h_t[f"AA{fila}"] = _toma
+                         pedidova01 = va01_2(0, l_ped_ext[i-1], l_dispones[i-1], fecha_entrega[i-1], l_mat_facturar, l_cant_facturar, l_convenios[i-1], l_mat_cliente_fact, tipo_dispone[0], centro_asistencial[0], l_canales[i-1], l_sectores[i-1])
+
+                         sleep(1)
+                         if pedidova01 != -1:
+                              print("\tYENDO A TOMA:", f"{pedidova01}", l_dispones[i-1], afiliado_anterior, l_observ_toma)
+                              _toma = toma(0, pedidova01, afiliado_anterior, l_dispones[i-1], l_observ_toma)
+                              #Completar Excel con pedido generado en TOMA
+                              for fila in l_filas:
+                                   h_t[f"AA{fila}"] = _toma
+                         else:
+                              print(f"NO SE PUDO CARGAR PEDIDO EN VA01 PARA AF: {afiliado_anterior}")
+                              # Completar Excel con pedido generado en VA01
+                              for fila in l_filas:
+                                   h_t[f"AA{fila}"] = pedidova01 + "cargado solamente en VA01"
 
                          l_observ_toma.clear()
                          l_mat_facturar.clear()
                          l_mat_cliente_fact.clear()
                          l_cant_facturar.clear()
+                         tipo_dispone.clear()
+                         centro_asistencial.clear()
                          l_filas.clear()
 
-                         print(f"AFILIADO:{afiliado_actual} |")
                          l_observ_toma.append(l_obsersevaciones_int[i])
                          l_mat_facturar.append(l_id_productos[i])
                          l_mat_cliente_fact.append(l_mat_cliente[i])
                          l_cant_facturar.append(l_cantidades_va01[i])
+                         tipo_dispone.append(l_tipo_dispone[i])
+                         centro_asistencial.append(l_ce_asist[i])
                          l_filas.append(filas_completar[i])
 
-                    elif i == len(l_afiliados_sap) - 1:
-                         print(f"{i} ------ ULTIMO AFILIADO: {afiliado_actual}")
-                         print("ULTIMA VUELTA VA01:",0, l_ped_ext[i], l_dispones[i], fecha_entrega[i], l_mat_facturar, l_cant_facturar, l_convenios[i], l_mat_cliente_fact)
-                         pedidova01 = va01_2(0, l_ped_ext[i], l_dispones[i], fecha_entrega[i], l_mat_facturar, l_cant_facturar, l_convenios[i], l_mat_cliente_fact)
-                         sleep(1)
-                         print("ULTIMA VUELTA TOMA:",0, "pedidova01", l_dispones[i], afiliado_actual, "06", l_observ_toma)
-                         _toma = toma(0, pedidova01, l_dispones[i], afiliado_actual, "06", l_observ_toma)
+                    if i == len(l_afiliados_sap) - 1:
+                         print(f"{i} - ULTIMO AFILIADO: {afiliado_actual}")
+                         print("\tULTIMA VUELTA VA01:",0, l_ped_ext[i], l_dispones[i], fecha_entrega[i], l_mat_facturar, l_cant_facturar, l_convenios[i], l_mat_cliente_fact, tipo_dispone[0], centro_asistencial[0])
 
-                         #Completar Excel con pedido generado
-                         for fila in l_filas:
-                              h_t[f"AA{fila}"] = _toma
-                         break
+                         pedidova01 = va01_2(0, l_ped_ext[i], l_dispones[i], fecha_entrega[i], l_mat_facturar, l_cant_facturar, l_convenios[i], l_mat_cliente_fact, tipo_dispone[0], centro_asistencial[0], l_canales[i], l_sectores[i])
+
+                         sleep(1)
+                         if pedidova01 != -1:
+                              print("\tULTIMA VUELTA TOMA:",0, "pedidova01", l_dispones[i], afiliado_actual, l_observ_toma)
+                              _toma = toma(0, pedidova01, afiliado_actual, l_dispones[i], l_observ_toma)
+                              #Completar Excel con pedido generado
+                              for fila in l_filas:
+                                   h_t[f"AA{fila}"] = _toma
+                         else:
+                              print(f"NO SE PUDO CARGAR PEDIDO EN VA01 PARA AF: {afiliado_anterior}")
+                              for fila in l_filas:
+                                   h_t[f"AA{fila}"] = pedidova01 + "cargado solamente en VA01"
+
                     print()
                     afiliado_anterior = afiliado_actual
 
@@ -144,10 +166,7 @@ def interfaz():
                excel_trabajo.save(rutas.archivo_excel_trabajo)
                excel_trabajo.close()
 
-
-
      botonCrear = Button(miFrame, text="Ejecutar", command=facturador)
-     # botonPdf.grid(row = 10, column = 2, sticky = "e", padx = 10, pady = 10)
      botonCrear.grid(row = 13, column = 2, sticky = "e", padx = 15, pady = 40)
 
      root.mainloop()
